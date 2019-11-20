41af0bbdfa92ec148e3585434d9c079b29660513
Hervé Hennes <nenes_fr@yahoo.fr>
2019-11-20 22:31:08 +0100

    New command : fop:console:debug
    Update Command with last reviews
    
    New Command : fop:console:debug

--- src/Commands/DebugMode.php
+++ src/Commands/DebugMode.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace FOP\Console\Commands\Debug;
+namespace FOP\Console\Commands;
 
 use FOP\Console\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -15,7 +15,7 @@ class DebugMode extends Command
     /**
      * @var array possible allowed dev mode passed in command
      */
-    const ALLOWED_COMMAND_STATES = ['enable', 'disable', 'toggle'];
+    const ALLOWED_COMMAND_STATES = ['status', 'enable', 'disable', 'toggle'];
 
     /**
      * {@inheritdoc}
@@ -23,12 +23,14 @@ class DebugMode extends Command
     protected function configure()
     {
         $this
-            ->setName('fop:console:debug:mode')
+            ->setName('fop:console:debug')
             ->setDescription('Configure debug mode')
-            ->setHelp('This command allows you to enable,disable or toggle debug mode')
-            ->addArgument('action', InputArgument::OPTIONAL,
-                'enable or disable debug mode ( possible values : ' . implode(",", self::ALLOWED_COMMAND_STATES) . ') ' . PHP_EOL,
-                'toggle'
+            ->setHelp('This command allows you to get or change debug mode')
+            ->addArgument(
+                'action',
+                InputArgument::OPTIONAL,
+                'enable or disable debug mode ( possible values : ' . implode(',', self::ALLOWED_COMMAND_STATES) . ') ',
+                'status'
             );
     }
 
@@ -42,29 +44,37 @@ class DebugMode extends Command
         $debugMode = new DebugAdapter();
         $isDebugModEnabled = $debugMode->isDebugModeEnabled();
 
-        //check if action is allowed
-        if ( !in_array($action,self::ALLOWED_COMMAND_STATES)){
+        if (!in_array($action, self::ALLOWED_COMMAND_STATES)) {
             $io->error('Action not allowed');
             return false;
         }
 
-        //Define Toggle action
+        //Status
+        if ($action == 'status') {
+            $io->note('Current debug mode : ' . ((true === $isDebugModEnabled) ? 'enabled' : 'disabled'));
+            return 0;
+        }
+
+        //Toggle Action
         if ($action == 'toggle') {
             (true === $isDebugModEnabled) ? $action = 'disable' : $action = 'enable';
         }
 
-        //Enable dev mode
+        //Enable action
         if ($action == 'enable') {
             $returnCode = $debugMode->enable();
-        } //Disable dev mode
+        }
+        //Disable action
         else {
             $returnCode = $debugMode->disable();
         }
 
         if ($returnCode === DebugAdapter::DEBUG_MODE_SUCCEEDED) {
-            $io->success('Debug mode updated with success');
+            $io->success('Debug mode ' . $action . 'd with success');
         } else {
             $io->error('An error occured while updating debug mode');
         }
+
+        return 0;
     }
 }
