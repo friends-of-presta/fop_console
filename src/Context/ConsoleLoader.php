087b5cd950360b56d11dc4d425c2a4266ef658bb
Hervé Hennes <nenes_fr@yahoo.fr>
2019-12-05 20:27:03 +0100

    #4 Define Copyright header

--- src/Context/ConsoleLoader.php
+++ src/Context/ConsoleLoader.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * 2019-present Friends of Presta community
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/MIT
+ *
+ * @author Friends of Presta community
+ * @copyright 2019-present Friends of Presta community
+ * @license https://opensource.org/licenses/MIT MIT
+ */
 
 namespace FOP\Console\Context;
 
