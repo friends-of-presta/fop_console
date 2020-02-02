d995fec0976a85306183ab4b6b044b955b9f1cda
SebSept <sebastienmonterisi@yahoo.fr>
2020-02-02 20:10:26 +0100

    implementation (tested on linux).

--- src/Commands/ClearCacheFiles.php
+++ src/Commands/ClearCacheFiles.php
@@ -21,7 +21,9 @@ use \RuntimeException;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\Process\Process;
+use PrestaShop\PrestaShop\Adapter\Debug\DebugMode as DebugAdapter;
 
 /**
  * This command replace the cache directory with an empty one.
@@ -47,7 +49,10 @@ final class ClearCacheFiles extends Command
         $io = new SymfonyStyle($input, $output);
 
         try {
+            // first : disable event dispatcher otherwise errors will happen
+            $this->getApplication()->setDispatcher(new EventDispatcher());
             $this->processChecks();
+            $this->deleteOldCacheDirectory(); // may exist if this command failed before
             $this->renameCurrentCacheDirectory();
             $this->createNewCacheDirectory(); // probably not needed
             $io->success('New Empty cache directory created. Old cache directory deleted.');
@@ -72,28 +77,30 @@ final class ClearCacheFiles extends Command
 
     private function renameCurrentCacheDirectory()
     {
-        $process = new Process("mv '{$this->getCacheDirectoryBasePath()}' '{$this->getCacheDirectoryOldPath()}'");
+        $process = new Process(['mv', $this->getCacheDirectoryBasePath(), $this->getCacheDirectoryOldPath()]);
         $process->run();
         $this->handleUnsucessfullProcess(__FUNCTION__, $process);
     }
 
     private function deleteOldCacheDirectory()
     {
-        $process = new Process(["rm -rf {$this->deleteOldCacheDirectory()}"]);
-        $process->run();
-        $this->handleUnsucessfullProcess(__FUNCTION__, $process);
+        if (file_exists($this->getCacheDirectoryOldPath())) {
+            $process = new Process(['rm', '-rf', $this->getCacheDirectoryOldPath()/*.'/'*/]); // final slash needed
+            $process->run();
+            $this->handleUnsucessfullProcess(__FUNCTION__, $process);
+        }
     }
 
     private function createNewCacheDirectory()
     {
         $cache_dir = $this->getCacheDirectoryBasePath().((new DebugAdapter())->isDebugModeEnabled() ? 'dev' :'prod');
-        $process = new Process(["mkdir $cache_dir"]);
+        $process = new Process(['mkdir', $cache_dir]);
         $process->run();
         $this->handleUnsucessfullProcess(__FUNCTION__, $process);
     }
 
     /**
-     * @return string Cache directory path without env final directory (eg. dev|prod) with trailing slash
+     * @return string Cache directory path without env final directory (eg. dev|prod) without trailing slash
      * @throws RuntimeException
      */
     private function getCacheDirectoryBasePath(): string
@@ -102,7 +109,7 @@ final class ClearCacheFiles extends Command
             throw new RuntimeException('Cache directory path not defined in _PS_CACHE_DIR_');
         }
 
-        return preg_replace('!prod|dev\/$!', '/', _PS_CACHE_DIR_);
+        return preg_replace('!\\'.DIRECTORY_SEPARATOR.'(prod|dev)\\'.DIRECTORY_SEPARATOR.'$!', '', _PS_CACHE_DIR_);
     }
 
     private function getCacheDirectoryOldPath(): string
@@ -113,8 +120,8 @@ final class ClearCacheFiles extends Command
     private function handleUnsucessfullProcess(string $__FUNCTION__, Process $process)
     {
         if (!$process->isSuccessful()) {
-            throw new RuntimeException("Error doing $__FUNCTION__ : "
-                .$process->getCommandLine()
+            throw new RuntimeException("Error doing $__FUNCTION__ : ".PHP_EOL
+//                .$process->getCommandLine().PHP_EOL
                 . " : " . $process->getErrorOutput());
         }
     }
