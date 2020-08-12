d477339f11c20256922c1bad6cf95d9962729233
Jean-François Viguier <jf.viguier@gmail.com>
2020-08-12 15:34:26 +0200

    Passage licence en AFL et fichier licence

--- fop_console.php
+++ fop_console.php
@@ -4,14 +4,17 @@
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the MIT License
- * that is bundled with this package in the file LICENSE
+ * This source file is subject to the Academic Free License 3.0 (AFL-3.0)
+ * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * https://opensource.org/licenses/MIT
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to us so we can send you a copy.
  *
  * @author Friends of Presta community
  * @copyright 2019-present Friends of Presta community
- * @license https://opensource.org/licenses/MIT MIT
+ * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
 require_once 'vendor/autoload.php';
 
