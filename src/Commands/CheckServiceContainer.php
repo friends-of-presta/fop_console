f4b9e38f4263cf2a79a379d8c3e7ba610d0813d9
SebSept <sebastienmonterisi@yahoo.fr>
2020-11-26 22:24:16 +0100

    license headers and file.

--- src/Commands/CheckServiceContainer.php
+++ src/Commands/CheckServiceContainer.php
@@ -1,17 +1,20 @@
 <?php
 /**
- * 2019-present Friends of Presta community
+ * Copyright (c) Since 2020 Friends of Presta
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the MIT License
- * that is bundled with this package in the file LICENSE
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file docs/licenses/LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * https://opensource.org/licenses/MIT
+ * https://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
  *
- * @author Friends of Presta community
- * @copyright 2019-present Friends of Presta community
- * @license https://opensource.org/licenses/MIT MIT
+ * @author    Friends of Presta <infos@friendsofpresta.org>
+ * @copyright since 2020 Friends of Presta
+ * @license   https://opensource.org/licenses/AFL-3.0  Academic Free License ("AFL") v. 3.0
  */
 
 namespace FOP\Console\Commands;
