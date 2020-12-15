344f72a742a2e134541f015b476ad80aa41bb612
SebSept <sebastienmonterisi@yahoo.fr>
2020-12-15 23:27:43 +0100

    ModuleTemplateOverrider : basic implementation.

--- src/Overriders/ModuleTemplateOverrider.php
+++ src/Overriders/ModuleTemplateOverrider.php
@@ -8,15 +8,28 @@ final class ModuleTemplateOverrider implements OverriderInterface
 {
     public function run(string $path): string
     {
-        $final_path = 'themes/classic/' . $path;
-        $fs = new Filesystem();
-        $fs->copy($path, $final_path);
+        try {
+            $final_path = sprintf('themes/%s/%s', $this->getThemePath(), $path);
+            $fs = new Filesystem();
+            $fs->copy($path, $final_path, true);
 
-        return "File $final_path created";
+            return "File $final_path created";
+        } catch (\Exception $exception) {
+            return 'An error occurred : ' . $exception->getMessage();
+        }
     }
 
     public function handle(string $path): bool
     {
         return fnmatch('modules/*/*.tpl', $path);
+    }
+
+    /**
+     * @return string
+     */
+    private function getThemePath(): string
+    {
+        // @todo Maybe it's better to rely on the directory property
+        return \Context::getContext()->shop->theme->getName();
     }
 }
