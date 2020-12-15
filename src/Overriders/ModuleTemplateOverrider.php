add2490ad30dfa17f2050009509457883b2d4563
SebSept <sebastienmonterisi@yahoo.fr>
2020-12-15 22:18:29 +0100

    ModuleTemplateOverrider POC.

--- src/Overriders/ModuleTemplateOverrider.php
+++ src/Overriders/ModuleTemplateOverrider.php
@@ -2,15 +2,21 @@
 
 namespace FOP\Console\Overriders;
 
+use Symfony\Component\Filesystem\Filesystem;
+
 final class ModuleTemplateOverrider implements OverriderInterface
 {
     public function run(string $path): string
     {
-        return '';
+        $final_path = 'themes/classic/' . $path;
+        $fs = new Filesystem();
+        $fs->copy($path, $final_path);
+
+        return "File $final_path created";
     }
 
     public function handle(string $path): bool
     {
-        return false;
+        return fnmatch('modules/*/*.tpl', $path);
     }
 }
