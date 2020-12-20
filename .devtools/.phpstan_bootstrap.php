698e50a5317d2e238c74ca5c331138cf40e79a64
SebSept <sebastienmonterisi@yahoo.fr>
2020-12-20 14:02:56 +0100

    workflow phpstan sans docker.

--- .devtools/.phpstan_bootstrap.php
+++ .devtools/.phpstan_bootstrap.php
@@ -1,26 +1,39 @@
 <?php
+/**
+ * Handle _PS_ROOT_DIR_ environment variable configuration.
+ *
+ * 1. Set it if needed
+ *
+ * It could be set before.
+ * If it is not, it can be set in __DIR__ . '/.phpstan_bootstrap_config.php'
+ *
+ * 2. Check it
+ */
 
-$filename = __DIR__.'/.phpstan_bootstrap_config.php';
-// check if config file exists and create a default one if needed.
-if(!file_exists($filename)) {
-    file_put_contents($filename,
-        '<?php'.PHP_EOL.'putenv(\'_PS_ROOT_DIR_=/path/to/prestashop/\');');
-    throw new \Exception(PHP_EOL.PHP_EOL.">>> Missing phpstan configuration file.".PHP_EOL."Default file created. Edit it : \"$filename\"");
+// -- 1. Set _PS_ROOT_DIR_ if needed
+
+$ps_root_env_file_path = __DIR__ . '/.phpstan_bootstrap_config.php';
+if (!getenv('_PS_ROOT_DIR_')) {
+    // check if config file exists and create a default one if needed.
+    if (!file_exists($ps_root_env_file_path)) {
+        file_put_contents($ps_root_env_file_path,
+            '<?php' . PHP_EOL . 'putenv(\'_PS_ROOT_DIR_=/path/to/prestashop/\');');
+        throw new Exception(PHP_EOL . PHP_EOL . ">>> Missing phpstan configuration file." . PHP_EOL . "Default file created. Edit it : \"$ps_root_env_file_path\"");
+    }
+
+    require($ps_root_env_file_path); // _PS_ROOT_DIR_ is set in this file
 }
 
-require($filename);
+// 2. Check _PS_ROOT_DIR_
 
-// test if the value is correct
-$ps_root_dir = getenv('_PS_ROOT_DIR_');
-
-// still the default
-if('/path/to/prestashop/' === $ps_root_dir) {
-    throw new \Exception(PHP_EOL.PHP_EOL.">>> You MUST configure the path to Prestashop in \"$filename\"".PHP_EOL.PHP_EOL);
+// abort if _PS_ROOT_DIR_ is not really defined
+if ('/path/to/prestashop/' === getenv('_PS_ROOT_DIR_')) {
+    throw new Exception(PHP_EOL . PHP_EOL . ">>> You MUST configure the path to Prestashop in \"$ps_root_env_file_path\" or check _PS_ROOT_DIR_" . PHP_EOL . PHP_EOL);
 }
 
-// not a valid path
-if(!file_exists($ps_root_dir)) {
-    throw new \Exception(PHP_EOL.PHP_EOL.">>> invalid path to prestashop. Edit \"$filename\"");
+// abort if _PS_ROOT_DIR_ is not a valid path
+if (!file_exists(getenv('_PS_ROOT_DIR_'))) {
+    throw new Exception(PHP_EOL . PHP_EOL . ">>> invalid path to prestashop. Provided : ".getenv('_PS_ROOT_DIR_')." Edit \"$ps_root_env_file_path\" or check _PS_ROOT_DIR_ ");
 }
 
-// everything is fine
+// if execution passes here, everything is fine
