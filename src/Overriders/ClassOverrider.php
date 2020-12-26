4f61f580cd55353b3d55f8cc0dafb3843e9a3543
SebSept <sebastienmonterisi@yahoo.fr>
2020-12-27 00:30:45 +0100

    Basic implementation of ClassOverrider.php

--- src/Overriders/ClassOverrider.php
+++ src/Overriders/ClassOverrider.php
@@ -2,6 +2,10 @@
 
 namespace FOP\Console\Overriders;
 
+use Laminas\Code\Generator\ClassGenerator;
+use Laminas\Code\Generator\FileGenerator;
+use Symfony\Component\Filesystem\Filesystem;
+
 class ClassOverrider extends AbstractOverrider implements OverriderInterface
 {
     /**
@@ -9,7 +13,29 @@ class ClassOverrider extends AbstractOverrider implements OverriderInterface
      */
     public function run(): array
     {
-        // TODO: Implement run() method.
+//        $s = ClassGenerator::fromReflection(new ClassReflection($this->getCoreClassName()));
+
+//        $meth = $s->getMethod('cleanPositions');
+//        $db = new \Laminas\Code\Generator\DocBlockGenerator('@inheritDoc');
+//        $meth->setDocBlock($db);
+//
+//        $o = new ClassGenerator();
+//        $o->setName('Feature');
+//        $o->setExtendedClass('FeatureCore');
+//        $o->addMethodFromGenerator($meth);
+
+        $generated_class = new ClassGenerator();
+        $generated_class->setName($this->getClassName())
+            ->setExtendedClass($this->getCoreClassName());
+
+        $fileGenerator = new FileGenerator();
+        $fileGenerator->setClass($generated_class);
+
+        $fs = new Filesystem();
+        $fs->dumpFile($this->getTargetPath(), $fileGenerator->generate());
+        $this->setSuccessful();
+
+        return ["File {$this->getTargetPath()} created"];
     }
 
     /**
@@ -17,7 +43,7 @@ class ClassOverrider extends AbstractOverrider implements OverriderInterface
      */
     public function handle(): bool
     {
-        // TODO: Implement handle() method.
+        return fnmatch('classes/**.php', $this->getPath());
     }
 
     /**
@@ -25,6 +51,29 @@ class ClassOverrider extends AbstractOverrider implements OverriderInterface
      */
     public function getDangerousConsequences(): ?string
     {
-        // TODO: Implement getDangerousConsequences() method.
+        $fs = new Filesystem();
+        if ($fs->exists($this->getTargetPath())) {
+            return "File {$this->getTargetPath()} will be overwritten.";
+        }
+
+        return null;
+    }
+
+    private function getTargetPath(): string
+    {
+        // after 'classes/' included - probably ready to handle absolute paths
+        $file_and_folder = substr($this->getPath(), (int) strrpos('classes/', $this->getPath()));
+
+        return sprintf('override/%s', $file_and_folder);
+    }
+
+    private function getClassName(): string
+    {
+        return basename($this->getPath(), '.php');
+    }
+
+    private function getCoreClassName(): string
+    {
+        return basename($this->getPath(), '.php') . 'Core';
     }
 }
