be022797a98b3a8297068dfa4b60f4212894fc5f
Arnaud Scoté <arnaud@infra.fr>
2021-03-22 10:55:34 +0100

    php cs fixer

--- src/Commands/InstallEnvForDev.php
+++ src/Commands/InstallEnvForDev.php
@@ -1,39 +1,34 @@
 <?php
-    /**
-     * Copyright (c) Since 2020 Friends of Presta
-     *
-     * NOTICE OF LICENSE
-     *
-     * This source file is subject to the Academic Free License (AFL 3.0)
-     * that is bundled with this package in the file docs/licenses/LICENSE.txt.
-     * It is also available through the world-wide-web at this URL:
-     * https://opensource.org/licenses/afl-3.0.php
-     * If you did not receive a copy of the license and are unable to
-     * obtain it through the world-wide-web, please send an email
-     * to license@prestashop.com so we can send you a copy immediately.
-     *
-     * @author    Friends of Presta <infos@friendsofpresta.org>
-     * @copyright since 2020 Friends of Presta
-     * @license   https://opensource.org/licenses/AFL-3.0  Academic Free License ("AFL") v. 3.0
-     */
-    
+/**
+ * Copyright (c) Since 2020 Friends of Presta
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file docs/licenses/LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    Friends of Presta <infos@friendsofpresta.org>
+ * @copyright since 2020 Friends of Presta
+ * @license   https://opensource.org/licenses/AFL-3.0  Academic Free License ("AFL") v. 3.0
+ */
+
 namespace FOP\Console\Commands;
 
 use FOP\Console\Command;
-use PrestaShop\PrestaShop\Adapter\Cache\CombineCompressCacheConfiguration;
 use PrestaShop\PrestaShop\Core\Crypto\Hashing;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 class InstallEnvForDev extends Command
 {
-    /**
-     *
-     */
     protected function configure()
     {
         $this->setName('fop:install-env-for-dev')
@@ -46,13 +41,14 @@ class InstallEnvForDev extends Command
         $this->addUsage('--modifycustomerpwd to change all customers password');
         $this->addOption('url', 'u', InputOption::VALUE_REQUIRED, 'url to set');
         $this->addOption('ssl', null, InputOption::VALUE_REQUIRED, 'Use ssl? default false');
-        $this->addOption('modifyemployeepwd', 'mep',InputOption::VALUE_OPTIONAL, 'Modify all employee BO password', false);
-        $this->addOption('modifycustomerpwd', 'mcp',InputOption::VALUE_OPTIONAL,'Modify all customers password', false);
+        $this->addOption('modifyemployeepwd', 'mep', InputOption::VALUE_OPTIONAL, 'Modify all employee BO password', false);
+        $this->addOption('modifycustomerpwd', 'mcp', InputOption::VALUE_OPTIONAL, 'Modify all customers password', false);
     }
-    
+
     /**
      * @param InputInterface $input
      * @param OutputInterface $output
+     *
      * @return int|void|null
      */
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -64,19 +60,19 @@ class InstallEnvForDev extends Command
         $dbi = \Db::getInstance();
         $crypto = new Hashing();
         $res = true;
-        
+
         //START MYSQL TRANSACTION
         $dbi->execute('START TRANSACTION;');
-        
+
         //Get options value
-        $ssl = (bool)$input->getOption('ssl');
+        $ssl = (bool) $input->getOption('ssl');
         $url = $input->getOption('url') ?? $helper->ask($input, $output, new Question('<question>Please, specify the url you want for your env</question>'));
-        $idShop = (int)$input->getOption('id_shop');
-        $modifyemployeepwd = (bool)$input->getOption('modifyemployeepwd');
-        $modifycustomerpwd = (bool)$input->getOption('modifycustomerpwd');
-        
+        $idShop = (int) $input->getOption('id_shop');
+        $modifyemployeepwd = (bool) $input->getOption('modifyemployeepwd');
+        $modifycustomerpwd = (bool) $input->getOption('modifycustomerpwd');
+
         $io->text('<info>Update table ps_configuration</info>');
-    
+
         //URL configuration
         $io->text(sprintf('<info>set value %s for configuration name : PS_SHOP_DOMAIN and PS_SHOP_DOMAIN_SSL</info>', $url));
         $where = sprintf('name in ("%s","%s")', 'PS_SHOP_DOMAIN', 'PS_SHOP_DOMAIN_SSL');
@@ -84,50 +80,48 @@ class InstallEnvForDev extends Command
             $where = sprintf('name in ("%s","%s") AND id_shop = %s', 'PS_SHOP_DOMAIN', 'PS_SHOP_DOMAIN_SSL', $idShop);
         }
         $res = $res && $dbi->update('configuration', ['value' => $url], $where, 0, false, false);
-        
+
         //SSL configuration
-        $io->text(sprintf('<info>set value %s for configuration name : PS_SSL_ENABLED_EVERYWHERE and PS_SSL_ENABLED</info>', (int)$ssl));
+        $io->text(sprintf('<info>set value %s for configuration name : PS_SSL_ENABLED_EVERYWHERE and PS_SSL_ENABLED</info>', (int) $ssl));
         $where = sprintf('name in ("%s", "%s")', 'PS_SSL_ENABLED_EVERYWHERE', 'PS_SSL_ENABLED');
         if ($idShop) {
             $where = sprintf('name in ("%s", "%s") AND id_shop = %s', 'PS_SSL_ENABLED_EVERYWHERE', 'PS_SSL_ENABLED', $idShop);
         }
-        $res = $res && $dbi->update('configuration', ['value' => (int)$ssl], $where, 0, false, false);
-        
+        $res = $res && $dbi->update('configuration', ['value' => (int) $ssl], $where, 0, false, false);
+
         //URL configuration in shop_url
         $io->text('<info>Update table ps_shop_url</info>');
         $where = sprintf('id_shop = %s', $idShop);
         $res = $res && $dbi->update('shop_url', ['domain' => $url, 'domain_ssl' => $url], $where, 0, false, false);
-    
-    
+
         //Regenerate htaccess
         $io->text('<info>Regenerate htaccess</info>');
         $command = $this->getApplication()->find('fop:generate:htaccess');
         $returnCode = $command->execute($input, $output);
         $res = $res && !$returnCode;
-    
+
         //Change Employee BO pwd
         if ($modifyemployeepwd) {
             $io->text('<info>Modify password for all BO employees</info>');
             $pwd = $helper->ask($input, $output, new Question($this->createQuestionString('Please, define password for all employee')));
             $res = $res && $dbi->update('employee', ['passwd' => $crypto->hash($pwd, _COOKIE_KEY_)], '', 0, false, false);
         }
-    
+
         //Change all customer pwd
         if ($modifycustomerpwd) {
             $io->text('<info>Modify password for all customers</info>');
             $pwd = $helper->ask($input, $output, new Question($this->createQuestionString('Please, define password for all customer')));
             $res = $res && $dbi->update('customer', ['passwd' => $crypto->hash($pwd, _COOKIE_KEY_)], '', 0, false, false);
         }
-        
+
         //debug mode on
         $io->text('<info>Active debug mode</info>');
         $debugMode = new \PrestaShop\PrestaShop\Adapter\Debug\DebugMode();
         $debugMode->enable();
-        
-        
+
         //cache off
         $io->text('<info>Disable cache</info>');
-        
+
         //smart cache js css
         $io->text('<info>Setup smart cache (js & css)</info>');
         $ccc = $this->getContainer()->get('prestashop.adapter.ccc.configuration');
@@ -136,14 +130,14 @@ class InstallEnvForDev extends Command
         $combineCacheConfig['smart_cache_js'] = false;
         $combineCacheConfig['apache_optimization'] = false;
         $res = $res && !$ccc->updateConfiguration($combineCacheConfig);
-        
+
         //global cache
         $io->text('<info>Setup global cache</info>');
         $cc = $this->getContainer()->get('prestashop.adapter.caching.configuration');
         $cachingConf = $cc->getConfiguration();
         $cachingConf['use_cache'] = false;
         $res = $res && !$cc->updateConfiguration($cachingConf);
-        
+
         //smarty cache
         $io->text('<info>Setup smarty cache</info>');
         $smc = $this->getContainer()->get('prestashop.adapter.smarty_cache.configuration');
@@ -154,30 +148,32 @@ class InstallEnvForDev extends Command
         $smartyCacheConf['caching_type'] = 'filesystem';
         $smartyCacheConf['clear_cache'] = 'everytime';
         $res = $res && !$smc->updateConfiguration($smartyCacheConf);
-        
+
         //Clear all cache
         $io->text('<info>Clear all cache</info>');
         $cacheClearChain = $this->getContainer()->get('prestashop.core.cache.clearer.cache_clearer_chain');
         $cacheClearChain->clear();
-        
-        if(!$res) {
+
+        if (!$res) {
             //If error ROLLBACK sql update
             $dbi->execute('ROLLBACK');
             $io->error('Error during setup');
+
             return 1;
         }
-        
+
         //If no error commit all sql update
         $dbi->execute('COMMIT;');
         $io->success('Setup finish correctly');
-    
+
         return 0;
     }
-    
+
     /**
      * @param string $question
      * @param string|null $default
      * @param string $separator
+     *
      * @return string
      */
     protected function createQuestionString(string $question, string $default = null, string $separator = ':'): string
