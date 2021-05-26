dc118f6aee58dcf7b6700d4ab8d1ec35b4e2b9d7
Okom3pom <contact@okom3pom.com>
2021-05-27 01:37:23 +0200

    Add argument

--- src/Commands/CleanCategory.php
+++ src/Commands/CleanCategory.php
@@ -21,6 +21,7 @@ namespace FOP\Console\Commands;
 use Category;
 use Configuration;
 use FOP\Console\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -29,84 +30,175 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 final class CleanCategory extends Command
 {
     /**
+     * @var array possible command
+     */
+    const ALLOWED_COMMAND = ['status', 'enable-noempty', 'disable-empty'];
+
+    /**
      * {@inheritdoc}
      */
     protected function configure()
     {
-        $this->setName('fop:clean-category')
+        $this->setName('fop:category')
             ->setDescription('Active or desactive categorys depending on products')
             ->setHelp('This command desactive final categories witout product, and active final catgories with product')
             ->addUsage('--exclude=[XX,YY,ZZ] (id_category separate by coma)')
+            ->addArgument(
+                'action',
+                InputArgument::OPTIONAL,
+                'enable or disable debug mode ( possible values : ' . $this->getPossibleActions() . ') ',
+                'status'
+            )
             ->addOption('id_lang', null, InputOption::VALUE_OPTIONAL, 'Id lang')
-            ->addOption('exclude', null, InputOption::VALUE_OPTIONAL, 'Ids Category to exclude')
-            ->addOption('active_only', null, InputOption::VALUE_OPTIONAL, 'Only active category')
-            ->addOption('desactive_only', null, InputOption::VALUE_OPTIONAL, 'Only desactive category');
+            ->addOption('exclude', null, InputOption::VALUE_OPTIONAL, 'Ids Category to exclude');
     }
 
-    /**
-     * @param InputInterface $input
-     * @param OutputInterface $output
-     *
-     * @return int
-     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
 
         $id_lang = $input->getOption('id_lang') ?? Configuration::get('PS_LANG_DEFAULT');
+        $exclude = $input->getOption('exclude') ? explode(',', $input->getOption('exclude')) : [];
+        $desactivedCategories = [];
+        $activedCategories = [];
+        $action = $input->getArgument('action');
 
-        try {
-            $exclude = $input->getOption('exclude') ? explode(',', $input->getOption('exclude')) : [];
-            $desactivedCategories = [];
-            $activedCategories = [];
+        switch ($action) {
+            case 'status':
+                try {
+                    $categories = Category::getCategories($id_lang, false, false);
 
-            $categories = Category::getCategories($id_lang, false, false);
+                    foreach ($categories as $categorie) {
+                        if (!in_array($categorie['id_category'], $exclude)) {
+                            if (!Category::getChildren($categorie['id_category'], $id_lang, false)) {
+                                $categorieToCheck = new Category($categorie['id_category']);
 
-            foreach ($categories as $categorie) {
-                if (!in_array($categorie['id_category'], $exclude)) {
-                    if (!Category::getChildren($categorie['id_category'], $id_lang, false)) {
-                        $categorieToCheck = new Category($categorie['id_category']);
+                                $NbProducts = $categorieToCheck->getProducts($id_lang, 1, 1);
 
-                        $NbProducts = $categorieToCheck->getProducts($id_lang, 1, 1);
-
-                        if (!$NbProducts && 0 != $categorieToCheck->active && !$input->getOption('active_only')) {
-                            $categorieToCheck->active = 0;
-                            if (!$categorieToCheck->update()) {
-                                throw new \Exception('Failed to update Category : ' . $categorieToCheck->name[$id_lang]);
+                                if (!$NbProducts && 0 != $categorieToCheck->active) {
+                                    $desactivedCategories[] = $categorieToCheck->name[$id_lang];
+                                } elseif ($NbProducts && 1 != $categorieToCheck->active) {
+                                    $activedCategories[] = $categorieToCheck->name[$id_lang];
+                                }
                             }
-                            $desactivedCategories[] = $categorieToCheck->name[$id_lang];
-                        } elseif ($NbProducts && 1 != $categorieToCheck->active && !$input->getOption('desactive_only')) {
-                            $categorieToCheck->active = 1;
-                            if (!$categorieToCheck->update()) {
-                                throw new \Exception('Failed to update Category : ' . $categorieToCheck->name[$id_lang]);
-                            }
-                            $activedCategories[] = $categorieToCheck->name[$id_lang];
                         }
                     }
+                } catch (\Exception $e) {
+                    $io->getErrorStyle()->error($e->getMessage());
+
+                    return 1;
                 }
-            }
-        } catch (\Exception $e) {
-            $io->getErrorStyle()->error($e->getMessage());
 
-            return 1;
+                if (!$desactivedCategories && !$activedCategories) {
+                    $io->title('No Categories to updated');
+
+                    return 0;
+                }
+
+                if ($desactivedCategories) {
+                    $io->title('You have categorie(s) actived without product active');
+                    $io->text(' -- Categories : ' . implode(',', $desactivedCategories));
+                    $io->text(' -- You can run `./bin/console fop:category disable-empty` to fix it');
+                }
+
+                if ($activedCategories) {
+                    $io->title('You have categorie(s) disabled but with product active in the categorie');
+                    $io->text(' -- Categories : ' . implode(',', $activedCategories));
+                    $io->text(' -- You can run `./bin/console fop:category enable-noempty` to fix it');
+                }
+
+                return 0;
+
+                break;
+            case 'disable-empty':
+                try {
+                    $categories = Category::getCategories($id_lang, false, false);
+
+                    foreach ($categories as $categorie) {
+                        if (!in_array($categorie['id_category'], $exclude)) {
+                            if (!Category::getChildren($categorie['id_category'], $id_lang, false)) {
+                                $categorieToCheck = new Category($categorie['id_category']);
+
+                                $NbProducts = $categorieToCheck->getProducts($id_lang, 1, 1);
+
+                                if (!$NbProducts && 0 != $categorieToCheck->active) {
+                                    $categorieToCheck->active = 0;
+                                    if (!$categorieToCheck->update()) {
+                                        throw new \Exception('Failed to update Category : ' . $categorieToCheck->name[$id_lang]);
+                                    }
+                                    $desactivedCategories[] = $categorieToCheck->name[$id_lang];
+                                }
+                            }
+                        }
+                    }
+                } catch (\Exception $e) {
+                    $io->getErrorStyle()->error($e->getMessage());
+
+                    return 1;
+                }
+
+                if (!$desactivedCategories && !$activedCategories) {
+                    $io->title('No Categories to updated');
+
+                    return 0;
+                }
+
+                if ($desactivedCategories) {
+                    $io->title('The following categories are desactived');
+                    $io->text(implode(',', $desactivedCategories));
+                }
+
+                return 0;
+                break;
+                case 'enable-noempty':
+                    try {
+                        $categories = Category::getCategories($id_lang, false, false);
+
+                        foreach ($categories as $categorie) {
+                            if (!in_array($categorie['id_category'], $exclude)) {
+                                if (!Category::getChildren($categorie['id_category'], $id_lang, false)) {
+                                    $categorieToCheck = new Category($categorie['id_category']);
+
+                                    $NbProducts = $categorieToCheck->getProducts($id_lang, 1, 1);
+
+                                    if ($NbProducts && 1 != $categorieToCheck->active) {
+                                        $categorieToCheck->active = 1;
+                                        if (!$categorieToCheck->update()) {
+                                            throw new \Exception('Failed to update Category : ' . $categorieToCheck->name[$id_lang]);
+                                        }
+                                        $activedCategories[] = $categorieToCheck->name[$id_lang];
+                                    }
+                                }
+                            }
+                        }
+                    } catch (\Exception $e) {
+                        $io->getErrorStyle()->error($e->getMessage());
+
+                        return 1;
+                    }
+
+                    if (!$activedCategories) {
+                        $io->title('No Categories to updated');
+
+                        return 0;
+                    }
+
+                    if ($activedCategories) {
+                        $io->title('The following categories are actived');
+                        $io->text(implode(',', $activedCategories));
+                    }
+
+                    return 0;
+                    break;
+            default:
+                $io->error("Action $action not allowed." . PHP_EOL . 'Possible actions : ' . $this->getPossibleActions());
+
+                return 1;
         }
+    }
 
-        if (!$desactivedCategories && !$activedCategories) {
-            $io->title('No Categories updated');
-
-            return 0;
-        }
-
-        if ($desactivedCategories) {
-            $io->title('Categories desactived');
-            $io->text(implode(',', $desactivedCategories));
-        }
-
-        if ($activedCategories) {
-            $io->title('Categories actived');
-            $io->text(implode(',', $activedCategories));
-        }
-
-        return 0;
+    private function getPossibleActions(): string
+    {
+        return implode(',', self::ALLOWED_COMMAND);
     }
 }
