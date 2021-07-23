99147115334a7ba6d6d8de96d8e2e278a56f5015
Arnaud <arnaud.scote@gmail.com>
2021-07-23 18:20:12 +0200

    ignore lines phpstan

--- src/Commands/DevSetupEnv.php
+++ src/Commands/DevSetupEnv.php
@@ -128,19 +128,19 @@ class DevSetupEnv extends Command
         $this->io->text('<info>Update table ps_configuration</info>');
 
         //URL configuration
-        $res = $res && $this->updateUrlConfiguration($idShop, $url);
+        $res = $res && $this->updateUrlConfiguration($idShop, $url);/** @phpstan-ignore-line */
 
         //SSL configuration
-        $res = $res && $this->updateSslConfiguration($idShop, $ssl);
+        $res = $res && $this->updateSslConfiguration($idShop, $ssl);/** @phpstan-ignore-line */
 
         //URL configuration in shop_url
-        $res = $res && $this->updateShopUrl($idShop, $url, $puri, $vuri);
+        $res = $res && $this->updateShopUrl($idShop, $url, $puri, $vuri);/** @phpstan-ignore-line */
 
         //Regenerate htaccess
         $this->io->text('<info>Regenerate htaccess</info>');
         $command = $this->getApplication()->find('fop:generate:htaccess');
         $returnCode = $command->execute($input, $output);
-        $res = $res && !$returnCode;
+        $res = $res && !$returnCode;/** @phpstan-ignore-line */
 
         //Change Employee BO pwd
         if ($modifyEmployeePwd) {
@@ -162,13 +162,13 @@ class DevSetupEnv extends Command
         $this->io->text('<info>Disable cache</info>');
 
         //smart cache js css
-        $res = $res && $this->disableSmartCacheJsAndCss();
+        $res = $res && $this->disableSmartCacheJsAndCss();/** @phpstan-ignore-line */
 
         //global cache
-        $res = $res && $this->disableGlobalCache();
+        $res = $res && $this->disableGlobalCache();/** @phpstan-ignore-line */
 
         //smarty cache
-        $res = $res && $this->disableSmartyCache();
+        $res = $res && $this->disableSmartyCache();/** @phpstan-ignore-line */
 
         //Clear all cache
         $this->io->text('<info>Clear all cache</info>');
