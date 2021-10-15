b1ce5549e65dd12ca55ee9a51134de966a462ee2
Hervé Hennes <nenes_fr@yahoo.fr>
2021-10-15 21:09:57 +0200

    Add missing final classes Fix Phpstan

--- src/Commands/DevSetupEnv.php
+++ src/Commands/DevSetupEnv.php
@@ -85,7 +85,7 @@ class DevSetupEnv extends Command
      * @param InputInterface $input
      * @param OutputInterface $output
      *
-     * @return int|void|null
+     * @return int
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
