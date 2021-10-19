6809cc049bb6ff98542254c48585a5b8216058af
Kaudaj <info@kaudaj.com>
2021-10-19 14:40:55 +0200

    Fix license headers

--- tests/Unit/bootstrap.php
+++ tests/Unit/bootstrap.php
@@ -17,6 +17,7 @@
  * @license   https://opensource.org/licenses/AFL-3.0  Academic Free License ("AFL") v. 3.0
  * 
  */
+
 define('_PS_IN_TEST_', true);
 define('_PS_ROOT_DIR_', dirname(__DIR__, 2));
 define('_PS_MODULE_DIR_', _PS_ROOT_DIR_ . '/tests/Resources/modules/');
