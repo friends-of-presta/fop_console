085bb829cd421ec6a532cd0b671fe51aebc565ac
SebSept <sebastienmonterisi@yahoo.fr>
2021-11-11 21:41:00 +0100

    Nettoyage
    Signed-off-by: SebSept <sebastienmonterisi@yahoo.fr>

--- .devtools/PhpStanCustomRule.php
+++ .devtools/PhpStanCustomRule.php
@@ -22,12 +22,10 @@ declare(strict_types=1);
 
 namespace FOP\Console\DevTools;
 
-use Doctrine\Common\Inflector\Inflector;
 use FOP\Console\Tests\Validator\PhpStanNamesConsistencyService;
 use PhpParser;
 use PhpParser\Node;
 use PhpParser\Node\Expr;
-use PhpParser\Node\Name;
 use PhpParser\Node\Stmt;
 use PhpParser\NodeDumper;
 use PHPStan\Analyser\Scope;
@@ -44,9 +42,6 @@ use PHPStan\Rules\RuleErrorBuilder;
 class PhpStanCustomRule implements Rule
 {
     const FOP_BASE_COMMAND_CLASS_NAME = \FOP\Console\Command::class;
-
-    /** @var int|null @todo rename */
-    private $setName_line;
 
     /** @var PhpParser\Node\Stmt\ClassMethod */
     private $node;
@@ -111,23 +106,9 @@ class PhpStanCustomRule implements Rule
 
         $commandDomain = '?'; // where should it be found ?
         $commandClassName = $scope->getClassReflection()->getName();
-        $commandServiceName = $this->getServiceNameForClass($commandClassName);
+        $this->validatorService->validateNames($commandClassName, $commandName);
 
-        $this->validatorService->ditBonjour();
-//        $validationResult = $this->validatorService->validate($commandDomain, $commandClassName, $commandName, $commandServiceName);
-
-//        dump($validationResult);
-//        echo ' wip Ready to check ... '.PHP_EOL;
         return [];
-
-        $class_name = (string) str_replace('FOP\Console\Commands\\', '', $scope->getClassReflection()->getName()); // with namespace amputé de \FOP\Console
-
-        return $errors;
-    }
-
-    private function getServiceNameForClass(string $className): string
-    {
-        return 'nop'; // @todo implement me
     }
 
     /**
@@ -163,8 +144,6 @@ class PhpStanCustomRule implements Rule
             return '';
         }
 
-        $this->setName_line = $setNameNode->getLine();
-
         // we might filter false positive by checking argument type.
         if ('Scalar_String' !== $setNameNode->args[0]->value->getType()) {
             $this->debug('setName() found but argument is not a string.');
@@ -173,88 +152,11 @@ class PhpStanCustomRule implements Rule
             return '';
         }
 
-//        $d = new NodeDumper(); // pour debug
-//        echo dump();
-//        throw new \Exception('ok si scalar_string mais sinon ?? cf Image\Abstract : proceder par reflexion ?');
-
         return $setNameNode->args[0]->value->value ?? '';
-    }
-
-    /**
-     * @param string $relative_file_path [Domain/]Command.php
-     * @param string $class_name Command
-     * @param string $command_name fop:[domain:]command
-     *                             - check command name against filename
-     *                             - check namespace against file directory
-     *                             - check classname against filename
-     *                             Some tests may be unnecessary because of psr-4
-     *
-     * @return array<string>|null null if ok; else  array of errors strings
-     *
-     * @throws \Exception
-     */
-    private function checkConsistency(string $relative_file_path, string $class_name, string $command_name): ?array
-    {
-        // extract command and domain from command
-        $command_command = ''; // command name extracted from $command_name
-        $command_domain = ''; // command domain extracted from $command_name
-
-        $matches = [];
-        if (preg_match('/fop:([a-z-]*):([a-z-]*)/', $command_name, $matches)) {
-            $command_command = array_pop($matches);
-            $command_domain = array_pop($matches);
-
-            // check that values are correctly extracted
-            $rebuild_command = sprintf('fop:%s:%s', $command_domain, $command_command);
-            if ($rebuild_command !== $command_name) {
-                dump($command_name, $command_command, $command_domain); /* @phpstan-ignore-error */
-                throw new \Exception('Failed to extract command parts');
-            }
-            unset($rebuild_command);
-        }
-
-        if (!$command_command && preg_match('/fop:([a-z-]*)/', $command_name, $matches)) {
-            $command_command = array_pop($matches);
-
-            // check that values are correctly extracted
-            $rebuild_command = sprintf('fop:%s', $command_command);
-            if ($rebuild_command !== $command_name) {
-                dump($command_name, $command_command, $command_domain);
-                throw new \Exception('Failed to extract command parts');
-            }
-            unset($rebuild_command);
-        }
-
-        // --- check command name against filename
-        $command_name_from_file_name = $this->getCommandNameFromFileName($relative_file_path);
-
-        if ($command_name_from_file_name !== $command_command) {
-            $errors[] = sprintf('Command name "%s" inconsistent with filename "%s"', $command_name, $relative_file_path);
-            $errors[] = sprintf('debug : Current command name "%s" is inconsistent with generated value "%s"', $command_command, $command_name_from_file_name);
-        }
-
-        // --- check namespace against file directory
-
-        // --- check classname against filename
-
-        return empty($errors) ? null : $errors;
-    }
-
-    /**
-     * @param string $relative_file_path [Domain/]CommandName.php
-     *
-     * @return string command-name
-     */
-    private function getCommandNameFromFileName(string $relative_file_path): string
-    {
-        $inflector = new Inflector();
-
-        return str_replace('_', '-', $inflector->tableize(basename($relative_file_path, '.php'))); // CommandName
     }
 
     private function nodeIsConfigureMethod(): bool
     {
-//        $this->debug(__FUNCTION__.' : '.$this->method->name->toString());
         return 'configure' === $this->node->name->toString();
     }
 
@@ -280,10 +182,4 @@ class PhpStanCustomRule implements Rule
         $nd = new NodeDumper();
         dump($nd->dump($node));
     }
-}
-
-class CommandName
-{
-    public $domain = '';
-    public $command = '';
 }
