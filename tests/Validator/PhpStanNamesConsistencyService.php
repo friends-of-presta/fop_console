652cdad01941b4d82ead53ebc6ec59c33bb71244
SebSept <sebastienmonterisi@yahoo.fr>
2021-11-15 23:14:40 +0100

    find service (in yaml configuration file) from classname
    Signed-off-by: SebSept <sebastienmonterisi@yahoo.fr>

--- tests/Validator/PhpStanNamesConsistencyService.php
+++ tests/Validator/PhpStanNamesConsistencyService.php
@@ -39,6 +39,9 @@ class PhpStanNamesConsistencyService
     /** @var \FOP\Console\Tests\Validator\FOPCommandFormatsValidator */
     private $validator;
 
+    /** @var array<string, string> */
+    private $servicesNamesCache;
+
     public function __construct(string $yamlServicesFilePath, FOPCommandFormatsValidator $validator)
     {
         $this->yamlServicesFilePath = $yamlServicesFilePath;
@@ -47,11 +50,15 @@ class PhpStanNamesConsistencyService
 
     public function validateNames(string $fullyQualifiedClassName, string $command): bool
     {
-        return $this->validator->validate(
-            $this->extractDomainFromFQCN($fullyQualifiedClassName),
-            $this->extractActionFromFQCN($fullyQualifiedClassName),
-            $command,
-            $this->getServiceNameForFQCN($fullyQualifiedClassName));
+        try {
+            return $this->validator->validate(
+                $this->extractDomainFromFQCN($fullyQualifiedClassName),
+                $this->extractActionFromFQCN($fullyQualifiedClassName),
+                $command,
+                $this->getServiceNameForFQCN($fullyQualifiedClassName));
+        } catch (\Exception $exception) {
+            throw new \RuntimeException(__CLASS__ . ' Internal error : ' . $exception->getMessage());
+        }
     }
 
     /**
@@ -64,7 +71,56 @@ class PhpStanNamesConsistencyService
 
     private function getServiceNameForFQCN(string $fullyQualifiedClassName): string
     {
-        return 'implement me';
+        $services = $this->getServicesNames();
+        if (!isset($services[$fullyQualifiedClassName])) {
+            dump($fullyQualifiedClassName, $services);
+            throw new \Exception('Service not found in service.yaml. Maybe unsupported syntax...');
+        }
+
+        return $services[$fullyQualifiedClassName] ?? '';
+    }
+
+    /**
+     * @return array<string, string> index : className, value : serviceName
+     *
+     * @todo the direct form of commands declaration are found, not the long form
+     * Recognized :
+     * ```yaml
+     *   fop.console.module.non_essential.command:
+     *     class: FOP\Console\Commands\Module\ModuleNonEssential
+     *     tags: [ console.command ]
+     *```
+     * Not recognized :
+     * ```yaml
+     * tags:
+     *  - {name: console.command}
+     * ```
+     */
+    private function getServicesNames(): array
+    {
+        if (is_null($this->servicesNamesCache)) {
+            $yaml = Yaml::parseFile($this->yamlServicesFilePath);
+            if (!isset($yaml['services'])) {
+                throw new \RuntimeException('Unexpected Symfony config file content : "services" section not found.');
+            }
+            $filterServicesWithConsoleTag = static function (array $service) {
+                return isset($service['tags']) && in_array('console.command', $service['tags']); // direct form
+            };
+            $commands = array_filter($yaml['services'], $filterServicesWithConsoleTag);
+            $servicesWithServiceField = array_map(static function (string $service, array $classDefinition) {
+                return [
+                    'service' => $service,
+                    'class' => $classDefinition['class'],
+                ];
+            },
+                array_keys($commands),
+                $commands
+            );
+
+            $this->servicesNamesCache = array_column($servicesWithServiceField, 'service', 'class');
+        }
+
+        return $this->servicesNamesCache;
     }
 
     /**
